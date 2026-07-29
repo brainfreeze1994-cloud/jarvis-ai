39fd80c731968dbb37005ab1eb7598d8d2041a63
brainfreeze1994-cloud <brainfreeze1994@gmail.com>
2026-07-29 21:38:23 +0400

    Update jarvis.js

--- api/jarvis.js
+++ api/jarvis.js
@@ -1,4 +1,4 @@
-------------------------------------------------------------------------------------------------------------------------
+
 // @vercel/node
 module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
